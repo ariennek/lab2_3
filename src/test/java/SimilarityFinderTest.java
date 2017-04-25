63def0d244887e54f8ae7b2109a956da26f6adea
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-04-25 04:28:14 +0200

    Dodano test calculateJackardSimilarityForOneEmptySequencee sprawdzajacy wynik gdy jedna sekwencja jest pusta
    Signed-off-by: zimny.krzysztof <180318@edu.p.lodz.pl>

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -28,4 +28,13 @@ public class SimilarityFinderTest {
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertThat(result,is(expected));
     }
+
+    @Test
+    public void calculateJackardSimilarityForOneEmptySequence() throws Exception {
+        int[] seq1 = {1,2,3};
+        int[] seq2 = {};
+        final double expected = 0.0d;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result,is(expected));
+    }
 }
