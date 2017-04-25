515b89e3a492296cc2cc3adb568e44d6dd22dc62
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-04-25 04:25:29 +0200

    Dodano test calculateJackardSimilarityForEmptySequences sprawdzajacy wynik dla dwoch pustych sekwencji
    Signed-off-by: zimny.krzysztof <180318@edu.p.lodz.pl>

--- src/test/java/SimilarityFinderTest.java
+++ src/test/java/SimilarityFinderTest.java
@@ -1,6 +1,10 @@
 import edu.iis.mto.similarity.FakeSequenceSearcher;
 import edu.iis.mto.similarity.SimilarityFinder;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Krzysztof Zimny on 4/25/2017.
@@ -14,5 +18,14 @@ public class SimilarityFinderTest {
     public void setUp() throws Exception {
         fakeSequenceSearcher = new FakeSequenceSearcher();
         similarityFinder = new SimilarityFinder(fakeSequenceSearcher);
+    }
+
+    @Test
+    public void calculateJackardSimilarityForEmptySequences() throws Exception {
+        int[] seq1 = {};
+        int[] seq2 = {};
+        final double expected = 1.0d;
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result,is(expected));
     }
 }
